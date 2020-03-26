6d567ab0459629b59c78bb352d81312b09f1f709
Chaklam <chaklam@ait.asia>
2020-03-26 10:57:49 +0700

    Remove redundant folders

--- Spring-Boot-Hibernate/bin/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into user (username, password, active, role) values ('chaklam', '$2y$12$2LA4/IzwsfoF.SFtdxwJIus48N6JwFzTdMwlrc9lXRHnA9EOBU7AS', true, 'ROLE_ADMIN');
-insert into user (username, password,  active, role) values ('john', '$2y$10$uwslZFS.czXR6VE7XzyBTuH.xGtdDmBBnioj2J/KZqJr0cHDEr3fa', true, 'ROLE_USER');
-//insert into employee_info (employee_age, fname, lname, mname, user_id) values (20, 'Chaklam', 'Silpasuwanchai', ' ', 1);
-//insert into employee_info (employee_age, fname, lname, mname, user_id) values (69, 'John', 'Cahill', ' ', 2);
-//insert into address (city, house_no, street_address, zipcode, emp_id) values ('Bangkok', '30/6', 'Ramindra', '10220', 1);
-//insert into employee_info(employee_age, user_id) values (20, 1)
-insert into employee_info(employee_age) values (20)

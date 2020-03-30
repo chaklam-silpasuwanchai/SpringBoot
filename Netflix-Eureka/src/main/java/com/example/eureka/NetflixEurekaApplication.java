320f0a489b48c9944c19867a0fd84ec30fa447cc
Chaklam <chaklam@ait.asia>
2020-03-30 09:39:01 +0700

    First commit

---
+++ Netflix-Eureka/src/main/java/com/example/eureka/NetflixEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaApplication.class, args);
+	}
+
+}
---
+++ Netflix-Eureka/src/test/java/com/example/eureka/NetflixEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
